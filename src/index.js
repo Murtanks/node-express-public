3c21cee5f2a5e169b59fa602b4a3fdf020fbeba2
Jahiro Zamora <jahiro.zamora@gmail.com>
2023-04-20 15:59:20 -0400

    primerCommit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const userRouter = require('./routers/user');
+
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', userRouter);
+
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+// Connect to MongoDB
+mongoose
+.connect(process.env.MONGODB_URI, { useUnifiedTopology: true})
+.then(() => console.log('asdasd conectado desde MongoDB'))
+.catch(error => console.error(error));
+
+
+app.listen(port, () => {
+    console.log('Server is running on port', port);
+});
+
+module.exports = app;
